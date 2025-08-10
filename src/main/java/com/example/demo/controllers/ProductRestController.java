9d95fa6e25f2dcb7654a4fd393d6caf7c73aaf0e
gourav sharma <gourav.101sharma@gmail.com>
2025-08-10 18:22:23 +0530

    new

---
+++ src/main/java/com/example/demo/controllers/ProductRestController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+import com.example.demo.enteties.Product;
+import com.example.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductRestController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @RequestMapping(value = "/product/",method = RequestMethod.GET)
+   public List<Product> getProducts(){
+        return repository.findAll();
+    }
+
+    @RequestMapping(value = "/product/{id}",method = RequestMethod.GET)
+    public Product getProduct(@PathVariable("id") int id){
+        return repository.findById(id).get();
+    }
+
+    @RequestMapping(value = "/product/",method = RequestMethod.POST)
+    public Product createProduct(@RequestBody Product product){
+        return repository.save(product);
+    }
+
+    @RequestMapping(value = "/product/",method = RequestMethod.PUT)
+    public Product updateProduct(@RequestBody Product product){
+        return repository.save(product);
+    }
+
+    @RequestMapping(value = "/product/{id}",method = RequestMethod.DELETE)
+    public void deleteProduct(@PathVariable("id") int id){
+        repository.deleteById(id);
+    }
+}
--- src/main/java/com/example/demo/enteties/Product.java
+++ src/main/java/com/example/demo/enteties/Product.java
@@ -1,10 +1,17 @@
 package com.example.demo.enteties;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
 //c
 @Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
