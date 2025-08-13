72692ea53482624ca25fd8ffa23844e3bf3addce
gourav sharma <gourav.101sharma@gmail.com>
2025-08-13 23:38:06 +0530

    new

--- src/main/java/com/example/demo/enteties/Product.java
+++ src/main/java/com/example/demo/enteties/Product.java
@@ -4,9 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
-//c
+
 @Entity
 public class Product {
 
